ba8b37e3ca2602b96e6e7cf236c466dbb16cb328
yunartyom <yunartyom@mail.ru>
2023-11-05 20:11:53 +0600

    main page

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -20,7 +20,32 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <!-- small box -->
+                        <div class="small-box" style="background:#c2c4b6">
+                            <div class="inner">
+                                <h3 style="color:#343a40">Пост</h3>
+                                <p style="color:#343a40">Добавление поста</p>
+                            </div>
+                            <div class="icon">
+                                <i class="nav-icon fas fa-newspaper"></i>
+                            </div>
+                            <a href="{{ route('addPostPage') }}" class="small-box-footer">Перейти <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <!-- small box -->
+                        <div class="small-box" style="background:#f7f0c6">
+                            <div class="inner">
+                                <h3 style="color:#343a40">Продукт</h3>
+                                <p style="color:#343a40">Добавление продукта</p>
+                            </div>
+                            <div class="icon">
+                                <i class="nav-icon fas fa-list"></i>
+                            </div>
+                            <a href="{{ route('addProductPage') }}" class="small-box-footer">Перейти <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div><!-- /.container-fluid -->
         </section>
